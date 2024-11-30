7928b2a4bb32e6b512f030728632724034a0928f
Galaticash-Games <galaticash.games@gmail.com>
2024-11-30 13:27:58 -0700

    Create main.js

---
+++ JudgementBar/main.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+// Submit the text from the input box to be written to a file
+function submitInput() {
+  let inputBox = document.getElementById("test-input");
+  let text = inputBox.innerHTML;
+
+  let filename = "testWrite;
+  readFile(filename);
+  writeFile(filename, text);
+  readFile(filename);
+}
+
+// Try to read data from the given file
+function readFile(filename) {
+    fs.writeFile(filename, (err, data) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+    console.log("Read file: ");
+    console.log(data);
+  });
+}
+
+// Try writing the given data to the given file
+function writeFile(filename, text) {
+  fs.writeFile(filename, text (err) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+    console.log("Successfully updated!");
+  });
+}
